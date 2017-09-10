e9d13cbff5f9d2cab2f1fd874f8f174aa1bbf002
Dennis Magnusson <dennis@Dennis-MacBook-Pro.local>
2017-09-10 12:22:53 +0300

    Master Version

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
-
+    var sound : AVAudioPlayer!
+    
+    var soundsList = ["note1","note2","note3","note4","note5","note6","note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
 
 
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        
+        if let soundURL = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav"){
+            var mySound: SystemSoundID = 0
+            AudioServicesCreateSystemSoundID(soundURL as CFURL, &mySound)
+            AudioServicesPlaySystemSound(mySound)
+        }
         
     }
-    
-  
 
 }
 
